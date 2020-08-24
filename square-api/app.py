d80288f2f4a139432bf8a659816c6868513fd126
Jacky Angara <jackyangara@gmail.com>
2020-08-25 03:02:37 +0800

    fix bug sklearn version mismatch

--- square-api/app.py
+++ square-api/app.py
@@ -1,5 +1,5 @@
 import falcon
-
+from falcon_swagger_ui import register_swaggerui_app
 
 from api.resources.predict_resource import PredictResource
 from api.resources.reset_resource import ResetResource
@@ -19,3 +19,16 @@ api.add_route('/reset/data', ResetResource(), suffix='data')
 api.add_route('/reset/model', ResetResource(), suffix='model')
 api.add_route('/retrain', RetrainResource())
 api.add_route('/retrain/data', RetrainResource(), suffix='add_data')
+
+SWAGGERUI_URL = '/swagger'  # without trailing slash
+SCHEMA_URL = 'http://petstore.swagger.io/v2/swagger.json'
+
+page_title = 'Falcon Swagger Doc'
+favicon_url = 'https://falconframework.org/favicon-32x32.png'
+
+register_swaggerui_app(
+    api, SWAGGERUI_URL, SCHEMA_URL,
+    page_title=page_title,
+    favicon_url=favicon_url,
+    config={'supportedSubmitMethods': ['post'], }
+)
